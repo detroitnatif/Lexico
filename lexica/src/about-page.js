b82ba3ac08d72a9fb161a4a24d4e6a2afcc0f577
Dylan Mitchell <dylan.mitchell@duke.edu>
2024-02-25 20:07:59 +0100

    added home and about page

---
+++ lexica/src/about-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>Made by Tyler and Dylan</div>
+  );
+}
+
+export default AboutPage;
---
+++ lexica/src/home-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>Welcome to the Home Page!</div>
+  );
+}
+
+export default HomePage;
